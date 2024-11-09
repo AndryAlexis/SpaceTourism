cc1739d83d74b14ade2ecdcbc496c4d55c1eb67a
AndryAlexis <andry.html@gmail.com>
2024-11-09 17:11:58 +0100

    Github pages settings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "https://github.com/AndryAlexis/SpaceTourism",
 };
 
 export default nextConfig;
