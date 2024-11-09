9f218758b9a5609fd56e25da73d79d889e5e3dd3
AndryAlexis <andry.html@gmail.com>
2024-11-09 17:23:10 +0100

    no message

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",  // Enables static exports
   basePath: "https://github.com/AndryAlexis/SpaceTourism",
-  images: {
-    unoptimized: true,
-  }
 };
 
 export default nextConfig;
